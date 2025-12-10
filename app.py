f9a13ff35d6beab68e74b7c732eee240b57bc8da
thangchinhqtbmhg-create <thangchinhqtbmhg@gmail.com>
2025-12-10 11:03:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Nông Đức Thắng - Trường Tiểu học Yên Phong - ĐT: 0829777289</p>
 </div>
 """, unsafe_allow_html=True)
 
