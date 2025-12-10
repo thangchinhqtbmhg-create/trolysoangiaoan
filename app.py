1760e0e94414f8102fcc792cbda3ba15b299bfeb
thangchinhqtbmhg-create <thangchinhqtbmhg@gmail.com>
2025-12-10 11:06:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Nông Đức Thắng - Trường Tiểu học Yên Phong - ĐT: 0829777289</div>", unsafe_allow_html=True)
